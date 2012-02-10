90c1a887c5d8bf5a402c4defcbc8f0640bb0d2f0
Tobias Fritz <herr_fritz@hotmail.de>
2012-02-10 13:01:07 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maillist::Application.config.secret_token = 'fbe56449ddecf874ce52197e779ce14b089cf656fd23bad8041d19a6114f1a0db766568598ee90017ef987ec7dba484aa56e6bae7e175ec4ff6a5a17cd06ec31'
